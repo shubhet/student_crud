4ff85c832022d44fbeb2863186d6a6b7e97556e9
Shubham Gupta <shubhet@gmail.com>
2023-10-22 11:44:40 +0530

    Add @EcptionHandler in file

--- src/main/java/com/example/student/crud/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/student/crud/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.example.student.crud.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.student.crud.models.ApiResponse;
@@ -9,6 +10,7 @@ import com.example.student.crud.models.ApiResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	
+	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<ApiResponse> resourceNotFoundException(ResourceNotFoundException ex){
 		String message = ex.getMessage();
 		ApiResponse apiResponse = new ApiResponse(message,false);
